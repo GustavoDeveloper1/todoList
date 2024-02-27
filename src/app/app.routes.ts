cc0cf4eb0d318796d2c7a9a17711c20028f9c065
Gustavo.Rocha <devinvestidor02@gmail.com>
2024-02-27 08:58:31 -0300

    💄 estilizando buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ToDoListComponent } from './modules/to-do-list/to-do-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ToDoListComponent
+    }
+];
